bce41ff3597310c5702c871a235e24c7611fcd90
unknown <maichellereynolds2@gmail.com>
2023-01-15 20:33:27 -0800

    Tracking files

---
+++ client/main.js
@@ -0,0 +1,97 @@
+const complimentBtn = document.getElementById("complimentButton");
+const popUp = document.getElementById("popUp");
+
+const attemptBtn = document.getElementById("attempt");
+
+const postBtn = document.getElementById("postExample")
+
+const submitBtn = document.getElementById("numSubmit")
+
+
+
+const getCompliment = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(res => {
+            const data = res.data;
+            alert(data);
+    });
+};
+
+complimentBtn.addEventListener('click', getCompliment)
+
+
+
+
+
+
+
+const fortuneBtn = document.getElementById("fortuneButton")
+
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/fortune/")
+    .then(res => {
+     const data = res.data
+     alert(data)
+
+    });
+
+};
+
+fortuneBtn.addEventListener('click', getFortune)
+
+
+
+const getPopUp = () => {
+    axios.get("http://localhost:4000/api/popUp/")
+        .then(res => {
+            const data = res.data
+            window.open(data)
+        })
+};
+
+
+
+//popUp.addEventListener('click', getPopUp)
+
+const putAttemptBtn = () => {
+    axios.put("http://localhost:4000/api/attempt/:id")
+    .then(res => {
+        const data = res.data
+        alert(data)
+    })
+};
+
+attemptBtn.addEventListener('click', putAttemptBtn)
+
+
+const postBtnExample = () => {
+    axios.post("http://localhost:4000/api/postExample")
+        .then(res => {
+            const data = res.data
+            alert(data)
+        })
+};
+
+postBtn.addEventListener('click', postBtnExample)
+
+
+
+const submitionHandler = () => {
+    const numTxt = document.getElementById("numText") 
+    console.log('text',numTxt.value); 
+    const body = { num: numTxt.value }
+    axios.post("http://localhost:4000/api/postNumButton", body)
+    .then(res => {
+        const data = res.data
+        alert(data)
+    })
+}
+
+submitBtn.addEventListener('click', submitionHandler)
+
+
+
+
+
+
+
---
+++ server/controller.js
@@ -0,0 +1,69 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+
+
+
+
+    getFortune: (req, res) => {
+        const fortunes = ["A pleasant surprise is waiting for you", "Adventure can be real happiness", "Allow compassion to guide your decisions","Believe it can be done","Congratulations! You are on your way."]
+      
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortunes = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortunes);
+    },
+
+    getPopUp: (req, res) => {
+        let url = "https://www.hdwallpaper.nu/wp-content/uploads/2015/02/maxresdefault.jpg"
+
+        res.status(200).send(url)
+    },
+
+    putAttempt: (req, res) => {
+        const str = "Everyone can use buttons"
+        res.status(200).send(str)
+    },
+
+    postBtnExample: (req, res) => {
+        const str = "Trying to Post"
+        res.status(200).send(str)
+    },
+
+    postNumBtn: (req, res) => {
+        let { num } = req.body
+        const str = `${num}`
+        res.status(200).send(str)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
